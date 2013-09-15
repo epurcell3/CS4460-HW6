9f8acbce9c26407160ff2cbcc0aede74d548f970
ajamxb <ajalma08@gmail.com>
2013-09-15 18:41:15 -0400

    Made it so D3 now handles data point color change when mouse hovers over it.

--- hw6.js
+++ hw6.js
@@ -8,6 +8,9 @@ var height = 512;
 // Radius of the data points
 var radius = 3;
 
+// Scale used to increase the height of the data points
+var scale = 5;
+
 d3.csv("tour_de_france.csv", function(error, data) {
     if (error) {
         console.log(error);
@@ -16,6 +19,7 @@ d3.csv("tour_de_france.csv", function(error, data) {
         console.log(data);  //DEBUG: delete this later...
         dataset = data;
         generateSpeedGraph();
+
     }
 });
 
@@ -36,7 +40,7 @@ function generateSpeedGraph(){
         })
         .attr("cy", function(d) {
             if (d.average_speed != "0"){
-                var circleHeight = d.average_speed * 5;
+                var circleHeight = d.average_speed * scale;
                 return height - circleHeight;
             }
             else{
@@ -44,8 +48,27 @@ function generateSpeedGraph(){
             }
 
         })
-        .attr("r", radius);
+        .attr("r", radius)
+        .attr("fill", function(d) {
+            return "rgb(19,117,255)";
+        })
+        .on("mouseover", function() {
+            d3.select(this)
+                .attr("fill", "orange");
+        })
+        .on("mouseout", function(d) {
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .attr("fill", "rgb(19,117,255)");
+    });
+
+
+
+    //var lines = svg.selectAll("line")
+
 }
+
 
 
 
