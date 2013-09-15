2dc1298b1e2b293ef23e3640c31f40d72a2efaa9
ajamxb <ajalma08@gmail.com>
2013-09-15 17:28:22 -0400

    Made function that plots points in average speed graph.

--- hw6.js
+++ hw6.js
@@ -1,18 +1,44 @@
+// Stores the CSV data
 var dataset;
 
-d3.csv("hw6_data.csv", function(error, data) {
+// Dimensions for the svg space
+var width = 1400;
+var height = 512;
+
+// Radius of the data points
+var radius = 2;
+
+d3.csv("tour_de_france.csv", function(error, data) {
     if (error) {
         console.log(error);
     }
     else{
         console.log(data);  //DEBUG: delete this later...
         dataset = data;
-        var selection = d3.select("div").selectAll("p");
-        selection.data(dataset).enter()
-            .append("p")
-            .text(function(d,i){return "item # at " + i + "	is " + d.year;});
+        generateSpeedGraph();
     }
 });
+
+function generateSpeedGraph(){
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var circles = svg.selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle");
+
+    circles.attr("cx", function(d, i) {
+        return (i * 12.5) + 25;
+    })
+        .attr("cy", function(d, i) {
+            return height - d.average_speed;
+        })
+        .attr("r", radius)
+        .attr("fill", "blue");
+}
 
 
 
